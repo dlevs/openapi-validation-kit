691245788aa9e1e1d49c2eb2a170897fa256b5b5
dlevs <dlevett@hotmail.co.uk>
2021-10-11 09:08:49 +0100

    Calculate entire schema at once

--- src/index.ts
+++ src/index.ts
@@ -16,139 +16,114 @@ async function main() {
     path.join(__dirname, '../examples/petstore.json')
   )
 
-  const compileSchema: typeof compile = (schema, name) => {
-    return compile({ ...schema, definitions: spec.definitions }, name, {
+  //
+  const schemaEntries = Object.values(spec.paths!)
+    .flatMap((methods) => Object.values(methods))
+    .map((method: OpenAPIV2.OperationObject) => {
+      let responses = Object.entries(method.responses)
+        .map(([status, response]) => {
+          if (!response) return null
+
+          if ('$ref' in response) {
+            return null //TODO
+          }
+
+          let { schema } = response
+
+          if (!schema) {
+            return null
+          }
+
+          return [`${method.operationId}ResponseBody${status}`, schema]
+        })
+        .filter(Boolean)
+
+      const params: OpenAPIV2.SchemaObject = {
+        type: 'object',
+        required: [],
+        additionalProperties: false,
+        properties: {},
+      }
+      const query: OpenAPIV2.SchemaObject = {
+        type: 'object',
+        required: [],
+        additionalProperties: false,
+        properties: {},
+      }
+      const headers: OpenAPIV2.SchemaObject = {
+        type: 'object',
+        required: [],
+        additionalProperties: false,
+        properties: {},
+      }
+      let body: null | OpenAPIV2.SchemaObject = null
+
+      method
+        .parameters! // TODO: !
+        .forEach((param) => {
+          if ('$ref' in param) {
+            return null //TODO
+          }
+
+          const schema =
+            param.in === 'path'
+              ? params
+              : param.in === 'query'
+              ? query
+              : param.in === 'headers'
+              ? headers
+              : null
+
+          if (schema) {
+            schema.properties[param.name] = param
+            if (param.required) {
+              schema.required?.push(param.name)
+            }
+          } else if (param.in === 'body') {
+            body = param.schema
+          }
+        })
+
+      // TODO: Put these in an object schema or namespace...
+      // { GetUsers: { Params: ..., Query: ... }}
+      const methodSchemaObject = Object.fromEntries([
+        ['Params', params],
+        ['Query', query],
+        ['Headers', headers],
+        ['RequestBody', body ? body : { type: 'null' }], // TODO: Better type? Like `never`?
+        ...responses,
+      ])
+
+      return [
+        method.operationId,
+        {
+          type: 'object',
+          properties: methodSchemaObject,
+          required: Object.keys(methodSchemaObject),
+          additionalProperties: false,
+        },
+      ]
+    })
+
+  const schemaObject = Object.fromEntries(schemaEntries)
+
+  const types = await compile(
+    {
+      type: 'object',
+      properties: schemaObject,
+      required: Object.keys(schemaObject),
+      additionalProperties: false,
+      definitions: spec.definitions,
+    },
+    'Requests',
+    {
       bannerComment: false,
       format: false,
-    })
-  }
-
-  //
-  const lines = await Promise.all(
-    Object.values(spec.paths!)
-      .flatMap((methods) => Object.values(methods))
-      .flatMap(async (method: OpenAPIV2.OperationObject) => {
-        let responses = await Promise.all(
-          Object.entries(method.responses)
-            .map(([status, response]) => {
-              if (!response) return null
-
-              if ('$ref' in response) {
-                return null //TODO
-              }
-
-              let { schema } = response
-
-              if (!schema) {
-                return null
-              }
-
-              // if ('$ref' in schema) {
-              //   schema = { $ref: schema.$ref?.replace(/^#/, '. ')}
-              // }
-
-              if (schema.$ref) {
-                // https://github.com/bcherny/json-schema-to-typescript/issues/132
-                // TODO: Document
-                schema = {
-                  type: 'object',
-                  required: ['__REPLACE_ME__'],
-                  properties: { __REPLACE_ME__: { $ref: schema.$ref } }, // TODO: Relative refs?
-                  additionalProperties: false,
-                }
-              }
-
-              return compileSchema(
-                schema,
-                `${method.operationId}ResponseBody${status}`
-              )
-            })
-            .filter(Boolean)
-        )
-
-        // TODO: grim
-        responses = responses.map((response) =>
-          response.replace(
-            /\s*export\s+interface\s+(.*?)\s+\{\s*__REPLACE_ME__\s*:\s*(.+?)\s*\}/,
-            'export type $1 = $2\n'
-          )
-        )
-
-        const params: OpenAPIV2.SchemaObject = {
-          type: 'object',
-          required: [],
-          additionalProperties: false,
-          properties: {},
-        }
-        const query: OpenAPIV2.SchemaObject = {
-          type: 'object',
-          required: [],
-          additionalProperties: false,
-          properties: {},
-        }
-        const headers: OpenAPIV2.SchemaObject = {
-          type: 'object',
-          required: [],
-          additionalProperties: false,
-          properties: {},
-        }
-        let body: null | OpenAPIV2.SchemaObject = null
-
-        method
-          .parameters! // TODO: !
-          .forEach((param) => {
-            if ('$ref' in param) {
-              return null //TODO
-            }
-
-            const schema =
-              param.in === 'path'
-                ? params
-                : param.in === 'query'
-                ? query
-                : param.in === 'headers'
-                ? headers
-                : null
-
-            if (schema) {
-              schema.properties[param.name] = param
-              if (param.required) {
-                schema.required?.push(param.name)
-              }
-            } else if (param.in === 'body') {
-              body = param.schema
-            }
-          })
-
-        const paramsTs = await compileSchema(
-          params,
-          `${method.operationId}Params`
-        )
-        const queryTs = await compileSchema(query, `${method.operationId}Query`)
-        // TODO: https://github.com/bcherny/json-schema-to-typescript/issues/132
-        const bodyTs =
-          body && !body.$ref // TODO
-            ? await compileSchema(body, `${method.operationId}RequestBody`)
-            : `export type ${method.operationId}RequestBody = never\n`
-        const headersTs = await compileSchema(
-          headers,
-          `${method.operationId}Headers`
-        )
-
-        return [
-          // export namespace ${method.operationId} {
-          paramsTs,
-          queryTs,
-          headersTs,
-          bodyTs,
-          responses.join('\n\n'),
-          // }
-        ].join('\n')
-      })
+    }
   )
 
   // TODO: Prettier really necessary?
-  const file = prettier.format(lines.join('\n'), {
+  const file = prettier.format(types, {
     semi: false,
     singleQuote: true,
     parser: 'typescript',
