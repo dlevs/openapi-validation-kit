f96aab6b1ff24bea8f8fd62d9c3783e47d2bebf8
Daniel Levett <dlevett@hotmail.co.uk>
2022-01-01 13:34:11 +0000

    Remove compiled output

--- out/types.js
+++
@@ -1,7 +0,0 @@
-// ------------------------------------------------------------------
-// Example file.
-//
-// This file is overwritten by when running the CLI. It exists for
-// development purposes, so we have types to work with.
-// ------------------------------------------------------------------
-export {};
